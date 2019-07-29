a035dc5279dad7a87dda2d00919f4a78e9502b80
Arnim <Arnim.Hanke@gmail.com>
2019-07-29 18:16:56 +0200

    More refactoring

--- Common/src/main/java/de/hanke/arnim/common/spielwiese/FilterElasticsearchSearchQuery.java
+++ Common/src/main/java/de/hanke/arnim/common/spielwiese/FilterElasticsearchSearchQuery.java
@@ -1,8 +1,8 @@
-package de.hank.arnim.common.spielwiese;
+package de.hanke.arnim.common.spielwiese;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.hank.arnim.common.Utils;
-import de.hank.arnim.common.ValueDto;
+import de.hanke.arnim.common.Utils;
+import de.hanke.arnim.common.ValueDto;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -10,7 +10,6 @@ import org.elasticsearch.client.RestHighLevelClient;
 import java.io.IOException;
 import java.util.*;
 
-import static de.hank.arnim.common.Utils.ADDRESS_ELASTICSEARCH;
 
 /**
  * Created by arnim on 12/29/17.
@@ -19,13 +18,15 @@ public class FilterElasticsearchSearchQuery {
 
     public static RestHighLevelClient client;
     public static ObjectMapper mapper;
-
-    static {
-        client = new RestHighLevelClient(RestClient.builder(new HttpHost(ADDRESS_ELASTICSEARCH, 9200, "http")));
+    public Utils utils;
+    public FilterElasticsearchSearchQuery() {
+        this.utils = new Utils();
+        client = new RestHighLevelClient(RestClient.builder(new HttpHost(utils.ADDRESS_ELASTICSEARCH, 9200, "http")));
         mapper = new ObjectMapper();
     }
 
     public static void main(String[] args) throws IOException {
+        FilterElasticsearchSearchQuery bla = new FilterElasticsearchSearchQuery();
         GregorianCalendar from = new GregorianCalendar(2019, Calendar.MARCH, 1, 0, 0);
         GregorianCalendar to = new GregorianCalendar(2019, Calendar.MARCH, 31, 23, 0);
         List<ValueDto> values = new ArrayList<>();
@@ -39,7 +40,7 @@ public class FilterElasticsearchSearchQuery {
         SearchResponse searchResponse = client.search(searchRequest);
         SearchHits hits = searchResponse.getHits();
 */
-        Map<String, List<ValueDto>> heizungssuite_iw_nhz_1 = Utils.getDataFromIndexInInterval(Collections.singletonList("heizungssuite_iw_nhz_1"), from.toInstant(), to.toInstant());
+        Map<String, List<ValueDto>> heizungssuite_iw_nhz_1 = bla.utils.getDataFromIndexInInterval(Collections.singletonList("heizungssuite_iw_nhz_1"), from.toInstant(), to.toInstant());
 
 /*        for (SearchHit hit : hits) {
             ValueDto valueDto = mapper.readValue(hit.getSourceAsString(), ValueDto.class);
